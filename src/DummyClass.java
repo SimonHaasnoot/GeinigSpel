05424444c29f510c14ea234eecec9f9861e04346
SimonHaasnoot <simonhaasnoot@hotmail.com>
2016-02-11 23:45:14 +0100

    Create random method

--- src/DummyClass.java
+++ src/DummyClass.java
@@ -1,5 +1,13 @@
+import java.util.Random;
+
 /**
  * Created by Simon on 2/11/2016.
  */
 public class DummyClass {
+
+    public static int getRandomNumber() {
+        Random rand = new Random();
+
+        return rand.nextInt(1000);
+    }
 }
