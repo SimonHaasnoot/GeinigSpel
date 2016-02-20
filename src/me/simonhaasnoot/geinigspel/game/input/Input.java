2c987b7daff64f98b7358d96eb37781fb5bc6b1b
Tim Visée <timvisee@gmail.com>
2016-02-20 20:57:00 +0100

    Created method to process a key event

--- src/me/simonhaasnoot/geinigspel/game/input/Input.java
+++ src/me/simonhaasnoot/geinigspel/game/input/Input.java
@@ -1,5 +1,7 @@
 package me.simonhaasnoot.geinigspel.game.input;
 
+import me.simonhaasnoot.geinigspel.game.input.key.KeyEventType;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,6 +29,30 @@ public class Input {
      */
     public static boolean isPressed(int keyCode) {
         return pressedKeys.contains(keyCode);
+    }
+
+    /**
+     * Process a key event type, such as a key press or release.
+     *
+     * @param keyCode The key code to check for, see constants in KeyEvent class.
+     * @param eventType The type of event.
+     */
+    public static void processKeyEvent(int keyCode, KeyEventType eventType) {
+        // Process the key
+        switch(eventType) {
+            case PRESSED:
+                // Add the key to the list if it isn't in the list already
+                if(!pressedKeys.contains(keyCode))
+                    pressedKeys.add(keyCode);
+                break;
+
+            case RELEASED:
+                // Remove the key from the list if it's currently in the list
+                pressedKeys.remove((Integer) keyCode);
+                break;
+
+            default:
+        }
     }
 
     /**
