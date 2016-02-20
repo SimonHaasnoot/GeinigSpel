66c149c84b1ec987db85ead58203f14ef48e5ad6
Tim Visée <timvisee@gmail.com>
2016-02-20 20:41:49 +0100

    Added JavaDocs to BaseLevel class methods

--- src/me/simonhaasnoot/geinigspel/game/level/BaseLevel.java
+++ src/me/simonhaasnoot/geinigspel/game/level/BaseLevel.java
@@ -4,7 +4,17 @@ import me.simonhaasnoot.geinigspel.game.GameStateManager;
 
 public abstract class BaseLevel {
 
+    /**
+     * Called when the level is started.
+     *
+     * @param gsm The GameStateManager instance.
+     */
     public abstract void start(GameStateManager gsm);
 
+    /**
+     * Called each time the level is updated.
+     *
+     * @param gsm The GameStateManager instance.
+     */
     public abstract void update(GameStateManager gsm);
 }
