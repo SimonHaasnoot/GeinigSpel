9a634c5724c66115e3288cdae0b99df59ac9422c
Tim Visée <timvisee@gmail.com>
2016-02-20 20:58:31 +0100

    Added key processing in the input key listener

--- src/me/simonhaasnoot/geinigspel/game/input/key/InputKeyListener.java
+++ src/me/simonhaasnoot/geinigspel/game/input/key/InputKeyListener.java
@@ -1,5 +1,7 @@
 package me.simonhaasnoot.geinigspel.game.input.key;
 
+import me.simonhaasnoot.geinigspel.game.input.Input;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -7,16 +9,19 @@ public class InputKeyListener implements KeyListener {
 
     @Override
     public void keyTyped(KeyEvent e) {
-
+        // Process the event in the input manager
+        Input.processKeyEvent(e.getKeyCode(), KeyEventType.TYPED);
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        // Process the event in the input manager
+        Input.processKeyEvent(e.getKeyCode(), KeyEventType.PRESSED);
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
+        // Process the event in the input manager
+        Input.processKeyEvent(e.getKeyCode(), KeyEventType.RELEASED);
     }
 }
