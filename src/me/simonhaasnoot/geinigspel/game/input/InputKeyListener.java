4210a8ee845cffd31d67be52fe3d09f706655f09
Tim Visée <timvisee@gmail.com>
2016-02-20 20:50:25 +0100

    Created base class of the input key listener

---
+++ src/me/simonhaasnoot/geinigspel/game/input/InputKeyListener.java
@@ -0,0 +1,22 @@
+package me.simonhaasnoot.geinigspel.game.input;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class InputKeyListener implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
